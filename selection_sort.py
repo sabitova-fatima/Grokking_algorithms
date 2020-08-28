a819469e29b45de36bc33ee100d77555224f576c
sabitova-fatima <sabitova.fatima@gmail.com>
2020-08-28 23:55:47 +0200

    adding selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,18 @@
-def find_smallest(list):
-    smallest = list[0]
+def find_smallest(my_list):
+    smallest = my_list[0]
     smallest_index = 0
 
-    for i in range(1, len(list)):
-        if list[i] < smallest:
-            smallest = list[i]
+    for i in range(1, len(my_list)):
+        if my_list[i] < smallest:
+            smallest = my_list[i]
             smallest_index = i
         return smallest_index
 
-def selection_sort(list):
+def selection_sort(my_list):
     new_list = []
-    for i in range(1, len(list)):
-        smallest = find_smallest
-        new_list.append(list.pop(smallest))
+    for i in range(len(my_list)):
+        smallest = find_smallest(my_list)
+        new_list.append(my_list.pop(smallest))
     return new_list
 
 print(selection_sort([5,3,6,2,10]))
