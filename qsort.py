cfcfac2c83e52dc273ca3930484ad7622153db32
= <sabitova.fatima@gmail.com>
2020-08-30 23:48:18 +0200

    adding qsort.py

---
+++ qsort.py
@@ -0,0 +1,11 @@
+def qsort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return qsort(less) + [pivot] + qsort(greater)
+
+print(qsort([10,5,2,3]))
