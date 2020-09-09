26a65adbd6561789624c2ad4095de9f15172b191
Fatima <sabitova.fatima@gmail.com>
2020-09-10 00:25:07 +0300

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -1,5 +1,8 @@
+/* Number of states to be covered */
+
 states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
 
+/* Availible stations */
 
 stations = {}
 
@@ -8,8 +11,6 @@ stations["ktwo"] = set(['wa', 'id', 'mt'])
 stations["kthree"] = set(['or', 'nv', 'ca'])
 stations["kfour"] = set(['nv', 'ut'])
 stations["kfive"] = set(['ca', 'az'])
-
-print(stations)
 
 final_stations = set()
 
